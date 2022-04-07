cd4a0407db5b8f429fb231a42fbc70c7c7197e4c
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:38:32 +0200

    Add files via upload

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,1 +1,36 @@
+function myFunction(){
 
+    var myMusic = {
+
+        001: {
+
+            artist: "Billy Joel",
+            title: "Piano Man",
+            release_year: 1973, 
+            formats: {
+
+                1: "CD",
+                2: "8T",
+                3: "LP"
+            },
+            gold: true
+        },
+        002: {
+            artist: "Michael Jackson",
+            title: "Thriller",
+            release_year: 1983,
+            formats: {
+
+                1: "CD",
+                2: "8T",
+                3: "LP"
+            },
+            platinum: true
+
+        }
+    }
+    return myMusic;
+}
+myFunction()[2];
+
+module.exports = myFunction;
