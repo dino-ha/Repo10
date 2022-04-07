6806fa3a992855f17b1271a0fd2fb7bff1188b8d
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:36:43 +0200

    Add files via upload

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,18 @@
+function myFunction(par){
+
+    const lion = {
+
+        "name": "Simba",
+        "legs": 4,
+        "tails": 1, 
+        "roar": "roar-roar"
+    };
+
+    delete lion[par];
+    return lion;
+}
+
+myFunction("roar");
+
+module.exports = { lion, myFunction };
 
