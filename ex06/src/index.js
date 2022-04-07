71cc599996131ee8975dc4b2e0d183e375820093
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:36:12 +0200

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,17 @@
+lion ={
+
+    name: "Simba",
+    legs: 4,
+    tails: 1
+};
+
+function myFunction(propName, propValue){
+
+    lion[propName] = propValue;
+    return lion;
+}
+
+myFunction("roar", "roar-roar");
+
+module.exports = { lion, myFunction };
 
