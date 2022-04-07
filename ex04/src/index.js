598df92932af26551c430d30f60496c631f9b674
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:35:00 +0200

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,22 @@
+players = {
+
+    7: "Muhamed Besic",
+    10: "Miralem Pjanic",
+    11: "Edin Dzeko"
+};
+
+function myFunction(myObj){
+
+    var playerNumber = myObj[10];
+    var player = "";
+    player = playerNumber;
+
+    return player;
+
+
+}
+
+module.exports = { players, myFunction};
+
+
 
