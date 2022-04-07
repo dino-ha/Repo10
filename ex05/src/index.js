7a3a40f2bfeff3113c8bc077bf38a38581388a3a
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:35:35 +0200

    Add files via upload

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,1 +1,16 @@
+school ={
 
+    name: "Arena",
+    location: "Sarajevo",
+    established: 2020
+};
+
+function myFunction(name){
+
+    school.name = name;
+    return school;
+}
+
+myFunction("Paragon");
+
+module.exports = { school, myFunction };
