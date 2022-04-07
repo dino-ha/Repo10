6ffbfb9dc8842558e70cac212f88bc15e52447a8
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:33:19 +0200

    Add files via upload

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,17 @@
+var myPet ={
+
+    name: "Rex",
+    species: "Dog",
+    legs: 4,
+    friends: ["John", "Mark"]
+};
+
+function myFunction(myObj){
+
+    return myObj;
+}
+
+console.log(myFunction(myPet));
+
+module.exports = { myPet, myFunction};
 
